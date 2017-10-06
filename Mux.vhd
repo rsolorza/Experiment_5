3cbe6b63e70866b6418dc83f19f17a2ebedfea73
rsolorza <rsolorza@calpoly.edu>
2017-10-06 15:44:18 -0700

    Adding in component files

---
+++ Mux.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 10/06/2017 03:39:53 PM
+-- Design Name: 
+-- Module Name: Mux - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Mux is
+--  Port ( );
+end Mux;
+
+architecture Behavioral of Mux is
+
+begin
+
+
+end Behavioral;
